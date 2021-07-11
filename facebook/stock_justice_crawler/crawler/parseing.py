2f6eef86b14d39a8f2fa83f3b70c9c97d9b4f96a
Calvin <calvinliaoyk@gmail.com>
2021-07-11 16:33:03 +0800

    date judgment

--- facebook/stock_justice_crawler/crawler/parseing.py
+++ facebook/stock_justice_crawler/crawler/parseing.py
@@ -1,21 +1,35 @@
 import re
+from datetime import datetime, timedelta
+
+
+def dateformatter(timestamp):
+
+    if re.search(r'分鐘|小時|2020年12月19日', timestamp):
+        mmdd = datetime.today().strftime('%m/%d')
+    elif re.search(r'昨天', timestamp):
+        mmdd = (datetime.today() - timedelta(1)).strftime('%m/%d')
+    elif re.match(r'[\d]{1,2}月[\d]{1,2}日', timestamp):
+        mmdd = re.sub(r'日', '', re.sub(r'月', '/', re.search(r'[\d]{1,2}月[\d]{1,2}日', timestamp).group()))
+    else:
+        print(f'error timestamp: {timestamp}')
+        
+    yyyymmdd = datetime.today().strftime('%Y/') + mmdd
+    date = datetime.strptime(yyyymmdd, "%Y/%m/%d")
+
+    return yyyymmdd, date
 
 
 def get_company_content(company, browser):
     
-    titles = browser.find_elements_by_css_selector("div[class='kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q']")
-    values = browser.find_elements_by_css_selector("div[class='o9v6fnle cxmmr5t8 oygrvhab hcukyx3x c1et5uql ii04i59q']")
-    # timestamps = browser.find_elements_by_css_selector(".b6zbclly.myohyog2.l9j0dhe7.aenfhxwr.l94mrbxd.ihxqhq3m.nc684nl6.t5a262vz.sdhka5h4")
+    all_cont = browser.find_elements_by_css_selector("div[class='rq0escxv l9j0dhe7 du4w35lb']")
     
-    # title
-    for title in titles:
-        if title.text != '':
-            content = title.text
+    for cont in all_cont:
+        sens = cont.text.split('\n')
+        for index, info in enumerate(sens):
             
-            if re.search(r'的真實價值', content):
-                sub_content = re.split(',|、|的真實價值', content)
+            if re.search(r'的真實價值', info):
+                sub_content = re.split(',|、|的真實價值', info)
                 for i, sub in enumerate(sub_content):
-
                     code = re.search(r'\d{4}', sub)                        
                     if code:
                         code = code.group()
@@ -23,43 +37,37 @@ def get_company_content(company, browser):
                         name = re.sub(r'.\n', '', name)
                         company[name]['Code'] = code
                         
-    # value            
-    for value in values:
-        if value.text != '':
-            infos = value.text.split('\n')
+                        # get timestamp
+                        for ts_index in range(5):
+                            if re.search(r'分鐘|小時|昨天|[\d]{1,2}月[\d]{1,2}日', sens[index - (ts_index + 1)]):
+                                ts = sens[index - (ts_index + 1)]
 
-            for i, info in enumerate(infos):
+                        yyyymmdd, date = dateformatter(ts)                            
+                        #company[name]['Timestamp'] = ts
+                        company[name]['Date'] = yyyymmdd
 
-                if re.search(r'的真實價值', info):
-                    code = re.search(r'\d{4}', info)
-                    if code:
-                        code = code.group()
-                        name = re.split(r'\(|（', info)[0].strip()
-                        name = re.sub(r'.\n', '', name)
-                        company[name]['Code'] = code
+            elif re.search(r'的 Ks =', info):
+                index = re.split('的 Ks =', info)
+                index_name = index[0].strip()
+                index_value = index[1].strip()
+                company[index_name]['Ks'] = index_value
 
-                elif re.search(r'的 Ks =', info):
-                    index = re.split('的 Ks =', info)
-                    index_name = index[0].strip()
-                    index_value = index[1].strip()
-                    company[index_name]['Ks'] = index_value
+            elif re.search(r'的 P / E =', info):
+                index = re.split('的 P / E =', info)
+                index_name = index[0].strip()
+                index_value = index[1].split('=')[-1].strip()
+                company[index_name]['P / E'] = index_value
 
-                elif re.search(r'的 P / E =', info):
-                    index = re.split('的 P / E =', info)
-                    index_name = index[0].strip()
-                    index_value = index[1].split('=')[-1].strip()
-                    company[index_name]['P / E'] = index_value
+            elif re.search(r'的 FRV =', info):
+                index = re.split('的 FRV =', info)
+                index_name = index[0].strip()
+                index_value = index[1].split('=')[-1].strip()
+                company[index_name]['FRV'] = index_value
 
-                elif re.search(r'的 FRV =', info):
-                    index = re.split('的 FRV =', info)
-                    index_name = index[0].strip()
-                    index_value = index[1].split('=')[-1].strip()
-                    company[index_name]['FRV'] = index_value
-
-                elif re.search(r'的 泡沫程度 =', info):
-                    index = re.split('的 泡沫程度 =', info)
-                    index_name = index[0].strip()
-                    index_value = index[1].split('=')[-1].strip()
-                    company[index_name]['泡沫程度'] = index_value
+            elif re.search(r'的 泡沫程度 =', info):
+                index = re.split('的 泡沫程度 =', info)
+                index_name = index[0].strip()
+                index_value = index[1].split('=')[-1].strip()
+                company[index_name]['泡沫程度'] = index_value
             
     return company
--- facebook/stock_justice_crawler/crawler/run.py
+++ facebook/stock_justice_crawler/crawler/run.py
@@ -4,10 +4,14 @@ import time
 import logging
 from logging_config import console_hdlr, file_hdlr
 from parseing import get_company_content
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import defaultdict
 from selenium import webdriver
-from selenium.common.exceptions import ElementNotVisibleException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import ElementNotVisibleException, StaleElementReferenceException
 
 log_path = '../log'
 exeday = datetime.now().strftime('%Y%m%d')
@@ -18,8 +22,17 @@ logger.addHandler(console_hdlr())
 logger.addHandler(file_hdlr(log_path, exeday))
 
 
-def last_timestamp(timestamp_list):
+def last_timestamp():
     
+    date_class = ".b6zbclly.myohyog2.l9j0dhe7.aenfhxwr.l94mrbxd.ihxqhq3m.nc684nl6.t5a262vz.sdhka5h4"
+    
+    try:
+        wait = WebDriverWait(browser, 1)
+        wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, date_class)))
+        timestamp_list = browser.find_elements_by_css_selector(date_class)
+    except(StaleElementReferenceException):
+        print('stale element reference: element is not attached to the page document')
+        
     timestamp = []
     
     for ts in timestamp_list:
@@ -31,50 +44,72 @@ def last_timestamp(timestamp_list):
     return last_timestamp
 
 
-def date_of_post():
+def dateformatter(timestamp):
+
+    if re.search(r'分鐘|小時|2020年12月19日', timestamp):
+        mmdd = datetime.today().strftime('%m/%d')
+    elif re.search(r'昨天', timestamp):
+        mmdd = (datetime.today() - timedelta(1)).strftime('%m/%d')
+    elif re.match(r'[\d]{1,2}月[\d]{1,2}日', timestamp):
+        mmdd = re.sub(r'日', '', re.sub(r'月', '/', re.search(r'[\d]{1,2}月[\d]{1,2}日', timestamp).group()))
+    else:
+        print(f'error timestamp: {timestamp}')
+        
+    yyyymmdd = datetime.today().strftime('%Y/') + mmdd
+    date = datetime.strptime(yyyymmdd, "%Y/%m/%d")
+
+    return yyyymmdd, date
+
+
+def date_of_post(start_date):
     
-    date_class = ".b6zbclly.myohyog2.l9j0dhe7.aenfhxwr.l94mrbxd.ihxqhq3m.nc684nl6.t5a262vz.sdhka5h4"
     last_height = browser.execute_script("return document.body.scrollHeight")
     time.sleep(2)
-
-    timestamp_list = browser.find_elements_by_css_selector(date_class)
-
-    last_ts = last_timestamp(timestamp_list)
     
-    if re.search(r'小時|2020年12月19日', last_ts):
-        return True 
+    # 最尾端時間戳記
+    last_ts = last_timestamp()
+    
+    # 日期判斷
+    yyyymmdd, date = dateformatter(last_ts)
+    
+    # 截止日判斷
+    if date >= start_date:
+        return True
     else:
-        new_height = last_height + 500
+        print(date)
+        new_height = last_height + 1000
         browser.execute_script(f"window.scrollTo(0, {new_height});")
-        timestamp_list_check = browser.find_elements_by_css_selector(date_class)
-        last_ts = last_timestamp(timestamp_list_check)
+        last_ts = last_timestamp()
         
-        if re.search(r'小時|2020年12月19日', last_ts):
+        if date > start_date:
             return True
-        else:    
+        else:
             return False
 
 
 def see_more():
-
+    
     click_string = "div[class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl oo9gr5id gpro0wi8 lrazzd5p'][role='button']"
 
     try:
+        wait = WebDriverWait(browser, 2)
+        wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, click_string)))
         read_button = browser.find_element_by_css_selector(click_string)
-        time.sleep(0.5)
+        browser.implicitly_wait(1)
         if read_button:
+            browser.implicitly_wait(2)
             read_button.click()
     except ElementNotVisibleException:
-        logger.info("Exception：element not visible")
+        print("Exception：element not visible")
     except Exception as e:
-        logger.info(e)
+        print(Exception, e) 
 
 
 def to_file(company):
     
     df_list = []
     for com in company.keys():
-        if len(company[com].keys()) == 5:
+        if len(company[com].keys()) == 6:
             company[com]['Name'] = com
             df_list.append(company[com])
 
@@ -134,4 +169,6 @@ if __name__ == '__main__':
     logger.info("Open browser")
     company = defaultdict(dict)
     time.sleep(5)
-    main()
+    
+    start_date = datetime.today()
+    main(start_date)
